c5e74e51a0bf0994e15166a6ce494c8e7f319dce
Alexandre Gressier <alexandre@gressier.dev>
2023-03-07 22:14:40 +0100

    Set up AWS Amplify

--- app/src/main/java/com/example/base/Application.kt
+++ app/src/main/java/com/example/base/Application.kt
@@ -1,8 +1,19 @@
 package com.example.base
 
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
+
 class Application : android.app.Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        try {
+            Amplify.configure(applicationContext)
+            Log.i("Application", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("Application", "Could not initialize Amplify", error)
+        }
     }
 }
